0a6ceb4fb94d4d2ea05e8387a30067dc56301575
Maarten <maarten.somers@gmail.com>
2017-11-03 16:04:07 +0100

    Ok definitely need to pass self

--- apps/rolluiken.py
+++ apps/rolluiken.py
@@ -24,7 +24,7 @@ class RolluikController(appapi.AppDaemon):
   ## MAIN LOGIC
   def rolluik_control_cb(self, entity, attribute, old, new, kwargs):
     self.log(self)
-    self.rolluik_controller(new, None)
+    self.rolluik_controller(self, new, None)
 
   def open_at_sunrise_cb(self, kwargs):
     # Check if we are in away mode
