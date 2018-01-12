cea939cf46577c025d2830760c6b8a3e881071be
Maarten <maarten.somers@gmail.com>
2018-01-12 22:07:03 +0100

    Re-implement harmony activity handling

--- apps/harmony.py
+++ apps/harmony.py
@@ -27,12 +27,11 @@ class HarmonyController(hass.Hass):
     # Immediately react if new state is "on"
     if new == "on": 
       # Activate cast audio activity
-      self.call_service("remote/turn_on", entity_id = "remote.harmony_hub", activity = "29225980")
+      self.call_service("input_select/select_option", entity_id = "input_select.harmony_activity", option = "Cast Audio")
       self.turn_off('switch.harmony_remote__spotify')
       self.turn_off('switch.harmony_remote__tv')
-    if new == "off":
-      # give Harmony activity some time to update
-      self.run_in(self.power_off_check, 15, activityName = 'Cast Audio')
+    if new == "off" && self.get_state("input_select.harmony_activity") == "Cast Audio":
+      self.call_service("input_select/select_option", entity_id = "input_select.harmony_activity", option = "Power Off")
 
 
 
@@ -40,12 +39,11 @@ class HarmonyController(hass.Hass):
     # Immediately react if new state is "on"
     if new == "on": 
       # Activate spotify activity
-      self.call_service("remote/turn_on", entity_id = "remote.harmony_hub", activity = "25430094")
+      self.call_service("input_select/select_option", entity_id = "input_select.harmony_activity", option = "Spotify")
       self.turn_off('switch.harmony_remote__cast_audio')
       self.turn_off('switch.harmony_remote__tv')
-    if new == "off":
-      # give Harmony activity some time to update
-      self.run_in(self.power_off_check, 15, activityName = 'Spotify')
+    if new == "off" && self.get_state("input_select.harmony_activity") == "Spotify":
+      self.call_service("input_select/select_option", entity_id = "input_select.harmony_activity", option = "Power Off")
 
 
 
@@ -53,17 +51,10 @@ class HarmonyController(hass.Hass):
     # Immediately react if new state is "on"
     if new == "on": 
       # Activate tv activity
+            self.call_service("input_select/select_option", entity_id = "input_select.harmony_activity", option = "Tv Kijken")
+
       self.call_service("remote/turn_on", entity_id = "remote.harmony_hub", activity = "25429995")
       self.turn_off('switch.harmony_remote__cast_audio')
       self.turn_off('switch.harmony_remote__spotify')
-    if new == "off":
-      # give Harmony activity some time to update
-      self.run_in(self.power_off_check, 15, activityName = 'TV Kijken')
-
-
-  def power_off_check(self, kwargs):
-    # Check current harmony state
-    harmony_activity = self.get_state(entity="remote.harmony_hub", attribute="current_activity")
-    if harmony_activity == kwargs['activityName']:
-      # Turn off
-      self.call_service("remote/turn_off", entity_id = "remote.harmony_hub")
+    if new == "off" && self.get_state("input_select.harmony_activity") == "Tv Kijken":
+      self.call_service("input_select/select_option", entity_id = "input_select.harmony_activity", option = "Power Off")
