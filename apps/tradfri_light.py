3b625ac48988c5f461035cb8bf6789e6bac25b6c
Maarten <maarten.somers@gmail.com>
2018-03-09 17:32:49 +0100

    Simplified tradfri controller

--- apps/tradfri_light.py
+++ apps/tradfri_light.py
@@ -8,8 +8,8 @@ class TradfriLight(hass.Hass):
 
   def switch_change_cb(self, entity, attribute, old, new, kwargs):
     # toggle coupled entity
-    self.log(new)
-    if new == "on":
-      self.call_service("zigate/raw_command", cmd = "0092", data = "02" + self.args["target"] + "010101")
-    if new == "off":
-      self.call_service("zigate/raw_command", cmd = "0092", data = "02" + self.args["target"] + "010100")
+      self.call_service("zigate/raw_command", cmd = "0092", data = "02" + self.args["target"] + "010102")
+    # if new == "on":
+    #   self.call_service("zigate/raw_command", cmd = "0092", data = "02" + self.args["target"] + "010101")
+    # if new == "off":
+    #   self.call_service("zigate/raw_command", cmd = "0092", data = "02" + self.args["target"] + "010100")
