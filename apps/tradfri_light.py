1be5c6e2074ccbf91f9e3304bfe3f9cde436f6ac
Maarten <maarten.somers@gmail.com>
2018-03-09 17:28:18 +0100

    Add state log

--- apps/tradfri_light.py
+++ apps/tradfri_light.py
@@ -8,6 +8,7 @@ class TradfriLight(hass.Hass):
 
   def switch_change_cb(self, entity, attribute, old, new, kwargs):
     # toggle coupled entity
+    self.log(new)
     if new == "on":
       self.call_service("zigate/raw_command", cmd = "0092", data = "02" + self.args["target"] + "010101")
     if new == "off":
