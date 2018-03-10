d9546a584b30e28f3adbaf52a31f7beb5d687be2
Unknown <maarten.somers@gmail.com>
2018-03-10 12:56:01 +0100

    check odl and new state

--- apps/tradfri_light.py
+++ apps/tradfri_light.py
@@ -8,6 +8,8 @@ class TradfriLight(hass.Hass):
 
   def switch_change_cb(self, entity, attribute, old, new, kwargs):
     # toggle coupled entity
+    self.log(old)
+    self.log(new)
     self.call_service("zigate/raw_command", cmd = "0092", data = "02" + self.args["target"] + "010102")
     # if new == "on":
     #   self.call_service("zigate/raw_command", cmd = "0092", data = "02" + self.args["target"] + "010101")
